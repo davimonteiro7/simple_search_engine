d3936d361e1920d6baa51eb133c51236c5650bd3
davimonteiro7 <davimonteiro7@gmail.com>
2020-12-23 11:39:13 -0300

    feat(routes): add poison and plug_cowboy dependencies, then code the first route '/'.

---
+++ lib/simple_search_engine/simple_search_engine_router.ex
@@ -0,0 +1,17 @@
+defmodule SimpleSearchEngine.Router do
+  use Plug.Router
+  use Plug.Debugger
+  require Logger
+
+  plug(Plug.Logger, log: :debug)
+  plug(:match)
+  plug(:dispatch)
+
+  get "/" do
+   send_resp(conn, 200, "Backend Container - root router")
+  end
+
+  match _ do
+    send_resp(conn, 404, "Page not found")
+  end
+end
--- mix.exs
+++ mix.exs
@@ -14,7 +14,7 @@ defmodule SimpleSearchEngine.MixProject do
   # Run "mix help compile.app" to learn about applications.
   def application do
     [
-      extra_applications: [:logger],
+      extra_applications: [:logger, :plug_cowboy, :poison],
       mod: {SimpleSearchEngine.Application, []}
     ]
   end
@@ -22,9 +22,9 @@ defmodule SimpleSearchEngine.MixProject do
   # Run "mix help deps" to learn about dependencies.
   defp deps do
     [
-      {:distillery, "~> 2.0"}
-      # {:dep_from_hexpm, "~> 0.3.0"},
-      # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:distillery, "~> 2.0"},
+      {:plug_cowboy, "~> 2.4.1"},  
+      {:poison, "~> 4.0.1"}, 
     ]
   end
 end
